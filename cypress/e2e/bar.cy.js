8f7eb311008df771bd4ce74c427e90fe188c3196
Georgiy Vlasov <wlasowegor@gmail.com>
2022-11-07 14:18:16 +0400

    Fix a test

--- cypress/e2e/bar.cy.js
+++ cypress/e2e/bar.cy.js
@@ -1,7 +1,7 @@
 describe('Bar website test', () => {
     it('can make an order', () => {
         cy.visit('http://localhost:8100')
-        cy.get('.menu > .item').contains('APA 10').click()
+        cy.get('.menu > .item').contains('APA 20').click()
         cy.get('.order > .item').should('be.visible')
     })
 })
